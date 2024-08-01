dbfdf282319d99fcc7a4db5e4f210e7475f27fed
JohanFlotten <johanflotten@gmail.com>
2024-08-01 14:32:51 +0200

    test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Test from "./components/Test1"
+import React from 'react';
+import { GiCat } from "react-icons/gi";
+
+function App() {
+
+  return (
+    <>
+    <h1>hello world!!!</h1>
+     <Test />
+     <GiCat />
+    </>
+  )
+}
+
+export default App
